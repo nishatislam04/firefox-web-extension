450702bcade4fac372ea79c54a86337398b21469
nishat islam <nishatislam3108@gmail.com>
2024-12-09 10:04:19 +0600

    add: vite finalize

--- nextify/background.js
+++ nextify/background.js
@@ -1,7 +1,60 @@
-import browser from "webextension-polyfill";
+import {
+	getAllStates,
+	getLocalItem,
+	reset,
+	setLocalItem,
+	toggleActiveClass,
+	transmitCommand,
+} from "../helper.js";
 
-console.log("Hello from the background!");
+const items = ["header", "sidebar", "footer"];
 
-browser.runtime.onInstalled.addListener((details) => {
-  console.log("Extension installed:", details);
+async function toggleItemState(key) {
+	const item = await getLocalItem(key);
+
+	if (Object.entries(item).length === 0) {
+		await setLocalItem({ [key]: true });
+	} else {
+		await setLocalItem({
+			[key]: item[key] === true ? false : true,
+		});
+	}
+}
+
+document.addEventListener("click", async function (e) {
+	if (e.target.id === "header") {
+		const name = "header";
+		await toggleItemState(name);
+		const item = await getLocalItem(name);
+
+		toggleActiveClass(item[name], e);
+		transmitCommand({ command: "header", state: item[name] });
+	}
+
+	if (e.target.id === "sidebar") {
+		const name = "sidebar";
+		await toggleItemState(name);
+		const item = await getLocalItem(name);
+
+		toggleActiveClass(item[name], e);
+		transmitCommand({ command: "sidebar", state: item[name] });
+	}
+
+	if (e.target.id === "footer") {
+		const name = "footer";
+		await toggleItemState(name);
+		const item = await getLocalItem(name);
+
+		toggleActiveClass(item[name], e);
+		transmitCommand({ command: "footer", state: item[name] });
+	}
+
+	if (e.target.id === "reset") {
+		await reset(items);
+	}
+});
+
+items.forEach(async (item) => {
+	const state = await getLocalItem(item);
+	if (state[item]) document.querySelector(`#${item}`).classList.add("active");
 });
---
+++ nextify/content.js
@@ -0,0 +1,41 @@
+import browser from "webextension-polyfill";
+import { getLocalItem } from "../helper";
+
+// console.log("Hello from the background!");
+
+// browser.runtime.onInstalled.addListener((details) => {
+//   console.log("Extension installed:", details);
+// });
+
+const items = ["header", "sidebar", "footer"];
+const selector = {
+	header: ".header_header__zJOD0.header_sticky__2ak2z",
+	sidebar: "main div.relative div.sticky",
+	footer: ".jsx-1021054789.footer_root__6F7g2",
+};
+
+function toggleVisibility(selector, state) {
+	const element = document.querySelector(selector);
+
+	if (element) element.style.display = !state ? "block" : "none";
+}
+
+browser.runtime.onMessage.addListener((request) => {
+	switch (request.command) {
+		case "header":
+			toggleVisibility(selector.header, request.state);
+			break;
+		case "sidebar":
+			toggleVisibility(selector.sidebar, request.state);
+			break;
+		case "footer":
+			toggleVisibility(selector.footer, request.state);
+			break;
+	}
+});
+
+items.forEach(async (item) => {
+	const state = await getLocalItem(item);
+
+	if (state[item]) toggleVisibility(selector[item], true);
+});
